9e243b541dc9e2b0768c4c20bee4b578d8d05240
813675583@qq.com <sd2775659>
2018-11-16 19:42:38 +0800

    面向对象第一课

---
+++ 全栈基础/oop/01.py
@@ -0,0 +1,16 @@
+class Student():
+    pass
+
+class Pythonstudent():
+    name = None
+    age = 18
+    course = 'Python'
+    def doHomework(self):
+        print("我正在写作业")
+
+        return None
+
+yueyue = Pythonstudent()
+print(yueyue.name)
+print(yueyue.age)
+yueyue.doHomework()
